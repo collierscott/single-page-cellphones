f090bc96633ab2ec03da23bcf619ad3b68445ae2
Scott Collier <scott@onlinespaces.com>
2015-09-19 20:11:52 -0500

    Add bootstrap validator to form

--- js/main.js
+++ js/main.js
@@ -60,6 +60,50 @@ $(function(){
         offset: '75%'
     });
 
+    $('#form').bootstrapValidator({
+        message: 'This value is not valid',
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+        fields: {
+            name: {
+                validators: {
+                    notEmpty: {
+                        message: 'This field is required and cannot be empty'
+                    }
+                }
+            },
+            email: {
+                validators: {
+                    notEmpty: {
+                        message: 'This field is required and cannot be empty'
+                    },
+                    emailAddress: {
+                        message: 'The input is not a valid email address'
+                    }
+                }
+            },
+            message: {
+                validators: {
+                    notEmpty: {
+                        message: 'This message is required and cannot be empty'
+                    },
+                }
+            }
+        }
+    }).on('success.form.bv', function(e){
+        e.preventDefault();
+
+        var $form = $(e.target);
+        var bv = $form.data('bootstrapValidator');
+
+        $.post($form.attr('action'), $form.serialize(), function(result){
+            console.log(result);
+        }, 'json');
+    })
+
 });
 
 smoothScroll.init({
